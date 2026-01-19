3389a481a47e4f5a0c4ecd965e03d812b8f01e86
Ivo Hoolwerf <adm.Ivo.Hoolwerf@legerdesheils.nl>
2026-01-19 21:52:12 +0100

    Mimo pokedex in functions gieten: lijstje van pokemons en specifieke pokemon data

--- API/mimo_pokedex.py
+++ API/mimo_pokedex.py
@@ -1,13 +1,37 @@
 import requests
+import pandas as pd
 
-url = "https://pokedex.mimo.dev/api/pokemon/pikachu"
+base_url = "https://pokedex.mimo.dev/api/pokemon/"
 
-response = requests.get(url)
+def get_pokemons():
+  """"
+  Haal alle Pokemons op uit de Mimo Pokedex API en print ze als DataFrame
+  """
+  response = requests.get(base_url)
+  if response.status_code == 200: 
+    print("Pokemons successfully retrieved")
+    pokemon_data = response.json()
+    df = pd.DataFrame(pokemon_data)
+    print(df)
+  else:
+    print("Failed to retrieve Pokemons")
 
-if response.status_code == 200: 
-  print("Data successfully retrieved")
-  pokemon_data = response.json()
-  print(f"Name:{pokemon_data['name']}")
-  print(f"ID:{pokemon_data['id']}")
-else:
-  print("Failed to retrieve data")
+
+def get_pokemon(pokemon):
+  """
+  Haal een specifieke Pokemon op uit de Mimo Pokedex API en print de naam en ID
+
+  Args:
+      pokemon (str): Naam of ID van de Pokemon
+  """
+  response = requests.get(base_url+pokemon)
+  if response.status_code == 200: 
+    print("Pokemon successfully retrieved")
+    pokemon_data = response.json()
+    print(f"Name:{pokemon_data['name']}")
+    print(f"ID:{pokemon_data['id']}")
+  else:
+    print("Failed to retrieve Pokemon")
+
+#get_pokemons()
+get_pokemon("5")
